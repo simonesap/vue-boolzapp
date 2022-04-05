c6e2a79893115e8b247ae88998acdc37d2249601
Simone Saputo <simonesaputo0@gmail.com>
2022-04-05 15:47:44 +0200

    third-commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,8 +4,33 @@
 // La correzione avverrà in classe alle 9.30 del mattino, quindi ogni mattina ci troveremo in classe alle 9.30 (la presenza non è facoltativa).
 // Oggi iniziamo con la milestone 1:
 // Milestone 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+// dall’interlocutore (bianco) assegnando due classi CSS diverse
+// ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+// nome e immagine di ogni contatto
+// Milestone 2
+// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+// messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// ● Click sul contatto mostra la conversazione del contatto cliccato
+// Milestone 3
+// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
+// Milestone 4
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina)
+// Milestone 5 - opzionale
+// ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+// permette di cancellare il messaggio selezionato
+// ● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
+// Consigli utili:
+// ● Si possono trascurare le scrollbar verticali, sia nel pannello dei messaggi, che nella
+// lista dei contatti
+// ● I pulsanti e le icone possono non funzionare (a parte l’invio del messaggio)
+// ● Per gestire le date, può essere utile la libreria day.js
+// ● La struttura dell’array dei contatti potrebbe avere questa forma:
 
 var app = new Vue(
     {
@@ -17,6 +42,8 @@ var app = new Vue(
                 picture: '_io'
                 },
             ],
+            // contacts[indexOggetto].messages.message 
+            // questo metodo è per entrare in un oggetto di oggetto
             contacts: [
                 {
                     name: 'Michele',
@@ -201,16 +228,35 @@ var app = new Vue(
                     ],
                 }
             ],
-            newMessages: [],
-            newChat: [],
+            // created() {
+            //     console.log(day.js().minute);
+            //     let minute = day.js().minute;
+            // },
+        indexChanged: 0,
+        newChat: '',
+        status: 'sent'
         },
         methods: {
-            // openChat: function(){
-            //     this.newChat = {
 
-            //     }
-            // }
+            // Funzione per richiamare l'ultimo elemento di un array di oggetti
+            // lastHour: function(indexHour){
+            //     console.log(this.contact.messages.length());
+            //     this.indexHour = index;
+            // },
 
+            // Funzione che riconosce l'index dell'elemento ciclato
+            // e al click sul tag li mi cambia l'index,
+            // e inserendola nel tag img della chat di destra
+            // mi restituisce l'indice cliccato stampandolo a video
+            changeContact: function(indexClick){
+                console.log(indexClick);
+                // Creo la variabile ha come valore il parametro della 
+                // funzione chiamato nel forcioè index
+                this.indexChanged = indexClick;
+                console.log(`l'index è diventato: ${this.indexChanged}`);
+            },
+            
+            
             newStringUser: function(){
                 let newStringObj = {
                     date: this.date_function(),
